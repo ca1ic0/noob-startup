2725d59dad9d82b98b485f8494521f133bef0073
cyberl0afing <1103977318@qq.com>
2024-05-13 18:14:01 +0800

    增加docker-compose文件

---
+++ script/start-up.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+[ ${USER} != root ] && echo "no root" && exit 0 
+#install software package
+apt update && \
+apt install \
+golang g++ gcc cmake git make clang clangd python-is-python3 python3-pip python3-venv \
+fzf btop timeshift httpie llvm \
+docker.io docker-compose \
+openssh-server \
+-y
+
+#systemcd 
+systemctl enable ssh 
+systemctl restart ssh 
+systemctl enable docker 
+systemctl restart docker
+
+#portainer
+bash ./docker.sh
+docker  && bash ./portainer.sh || "No Docker installed, please check the driver"
+
+# CUDA toolchain
+which nvidia-smi && ./cuda.sh || echo  "No nv-smi installed, please check the driver" 
+[ -z ${CUDA_HOME} ] && echo " No CUDA INSTALLED On This System! " || ./nv-container.sh   
+
+
